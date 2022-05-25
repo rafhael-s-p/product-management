dde6a0f2f615d4da991fab501b1203ea7d280f91
Rafhael <rafhael.pereira@gft.com>
2022-05-25 13:41:23 -0300

    just adding some java documentation

--- src/labs/pm/data/Product.java
+++ src/labs/pm/data/Product.java
@@ -20,11 +20,24 @@ import java.math.BigDecimal;
 import static java.math.RoundingMode.HALF_UP;
 
 /**
- *
+ * {@code Product} class represents properties and behaviours
+ * of products objects in the Product Management System.
+ * <br>
+ * Each product has an id, name and price
+ * <br>
+ * Each product can have a discount, calculated based on a
+ * {@link DISCOUNT_RATE discount rate}
+ * @version 4.0
  * @author rlop
  */
 public class Product {
     
+    /**
+     * A constant that defines a {@link java.math.BigDecimal} value
+     * of the discount rate
+     * <br>
+     * Discount rate is 10%
+    */
     public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
     private int id;
     private String name;
@@ -54,6 +67,12 @@ public class Product {
         this.price = price;
     }
     
+    /**
+     * Calculates discount based on a product price and
+     * {@link DISCOUNT_RATE discount rate}
+     * 
+     * @return a {@link java.math.BigDecimal} value of the discount
+    */
     public BigDecimal getDiscount() {
         return price.multiply(DISCOUNT_RATE).setScale(2, HALF_UP);
     }

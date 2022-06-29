026f957774df63beff0b315daf1a01bcfea1478e
Rafhael <rafhael.pereira@gft.com>
2022-06-29 15:57:46 -0300

    allow multiple reviews for a product

--- src/labs/pm/data/Drink.java
+++ src/labs/pm/data/Drink.java
@@ -17,7 +17,6 @@
 package labs.pm.data;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.time.LocalTime;
 
 /**
@@ -29,12 +28,12 @@ public final class Drink extends Product {
     Drink(int id, String name, BigDecimal price, Rating rating) {
         super(id, name, price, rating);
     }
-    
+
     @Override
     public BigDecimal getDiscount() {
         LocalTime now = LocalTime.now();
         return (now.isAfter(LocalTime.of(17, 30))
-                && now.isBefore(LocalTime.of(18, 30))) 
+                && now.isBefore(LocalTime.of(18, 30)))
                 ? super.getDiscount() : BigDecimal.ZERO;
     }
 
@@ -42,5 +41,5 @@ public final class Drink extends Product {
     public Product applyRating(Rating newRating) {
         return new Drink(getId(), getName(), getPrice(), newRating);
     }
-    
+
 }

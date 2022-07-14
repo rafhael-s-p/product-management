650bc6939ed28c95d840d178f07572e0706e5a00
Rafhael <rafhael.pereira@gft.com>
2022-07-14 14:45:51 -0300

    produce customized product reports

--- src/labs/pm/data/ProductManager.java
+++ src/labs/pm/data/ProductManager.java
@@ -24,6 +24,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -124,6 +125,19 @@ public class ProductManager {
             txt.append("\n");
         }
 
+        System.out.println(txt);
+    }
+    
+    public void printProducts(Comparator<Product> sorter) {
+        List<Product> productList = new ArrayList<>(products.keySet());
+        productList.sort(sorter);
+        StringBuilder txt = new StringBuilder();
+        
+        for (Product product : productList) {
+            txt.append(formatter.formatProduct(product));
+            txt.append("\n");
+        }
+        
         System.out.println(txt);
     }
 

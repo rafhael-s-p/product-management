f622a454cb37e8cd93592810beb2753fd9f080de
Rafhael <rafhael.pereira@gft.com>
2022-07-21 09:15:24 -0300

    modify product manager to use streams

--- src/labs/pm/data/ProductManager.java
+++ src/labs/pm/data/ProductManager.java
@@ -31,6 +31,8 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -89,15 +91,15 @@ public class ProductManager {
         List<Review> reviews = products.get(product);
         products.remove(product, reviews);
         reviews.add(new Review(rating, comments));
-        int sum = 0;
+        product = product.applyRating(
+                Rateable.convert(
+                        (int) Math.round(
+                                reviews.stream()
+                                        .mapToInt(r -> r.getRating().ordinal())
+                                        .average()
+                                        .orElse(0))));
 
-        for (Review review : reviews) {
-            sum += review.getRating().ordinal();
-        }
-
-        product = product.applyRating(Rateable.convert(Math.round((float) sum / reviews.size())));
         products.put(product, reviews);
-
         return product;
     }
 
@@ -111,47 +113,36 @@ public class ProductManager {
         txt.append(formatter.formatProduct(product));
         txt.append("\n");
 
-        Collections.sort(reviews);
-
-        for (Review review : reviews) {
-
-            txt.append(formatter.formatReview(review));
-            txt.append("\n");
-
-        }
-
         if (reviews.isEmpty()) {
             txt.append(formatter.getText("no.reviews"));
-            txt.append("\n");
+        } else {
+            txt.append(reviews.stream()
+                    .map(r -> formatter.formatReview(r) + "\n")
+                    .collect(Collectors.joining()));
         }
 
         System.out.println(txt);
     }
-    
-    public void printProducts(Comparator<Product> sorter) {
-        List<Product> productList = new ArrayList<>(products.keySet());
-        productList.sort(sorter);
+
+    public void printProducts(Predicate<Product> filter, Comparator<Product> sorter) {
         StringBuilder txt = new StringBuilder();
-        
-        for (Product product : productList) {
-            txt.append(formatter.formatProduct(product));
-            txt.append("\n");
-        }
-        
+
+        products.keySet()
+                .stream()
+                .sorted(sorter)
+                .filter(filter)
+                .forEach(p -> txt.append(formatter.formatProduct(p) + "\n"));
+
         System.out.println(txt);
     }
 
     public Product findProduct(int id) {
-        Product result = null;
 
-        for (Product product : products.keySet()) {
-            if (product.getId() == id) {
-                result = product;
-                break;
-            }
-        }
-
-        return result;
+        return products.keySet()
+                .stream()
+                .filter(p -> p.getId() == id)
+                .findFirst()
+                .orElseGet(() -> null);
     }
 
     private static class ResourceFormatter {
